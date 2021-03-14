62b84aeeb5396b5a195a9b9ec6842b083b961481
montymse <mse450@hotmail.com>
2021-03-14 01:11:14 +0100

    Boston Housing

---
+++ BostonHousing.py
@@ -0,0 +1,31 @@
+import numpy as np
+import torch
+import torch.nn as nn
+
+# Loading training data file
+FILE = "Data/data.pth"
+data = torch.load(FILE)
+
+input_size = data["input_size"]
+output_size = data["output_size"]
+model_state = data["model_state"]
+inputs = data["inputs"]
+targets = data["targets"]
+
+# Model
+model = nn.Linear(input_size, output_size)
+model.load_state_dict(model_state)
+model.eval()
+
+# Predicted values
+output = model(inputs)
+prediction = output.detach().numpy()
+
+# Target values
+targets = targets.numpy()
+
+# Concatinating and saving the predicted values from model and target values
+predictions_target = np.concatenate((prediction, targets), axis=1)
+
+np.savetxt("Data/predictions_and_targets.csv", predictions_target, delimiter=',', header="prediction,target",
+           comments="")
---
+++ train.py
@@ -0,0 +1,60 @@
+import numpy as np
+import torch
+import torch.nn as nn
+from torch.utils.data import TensorDataset
+from torch.utils.data import DataLoader
+
+# Loading data
+csv = np.genfromtxt('Data/housing.csv', delimiter="", dtype='float32')
+
+# Preparing data
+inputs = csv[:, 1:13]
+targets = csv[:, 13:]
+
+inputs = torch.from_numpy(inputs)
+targets = torch.from_numpy(targets)
+
+# Dataset and DataLoader
+train_ds = TensorDataset(inputs, targets)
+train_dl = DataLoader(dataset=train_ds, batch_size=8, shuffle=True)
+
+# Model
+model = nn.Linear(inputs.shape[1], 1)
+criterion = nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-6)
+
+# Training
+for epoch in range(1000):
+
+    # Train with batches of data
+    for xb, yb in train_dl:
+        # Generating predictions
+        pred = model(xb)
+
+        # Calculating loss
+        loss = criterion(pred, yb)
+
+        # Computing gradients
+        loss.backward()
+
+        # Updating parameters using gradients
+        optimizer.step()
+
+        # Resetting the gradients to zero
+        optimizer.zero_grad()
+
+    # Print the progress
+    if (epoch + 1) % 10 == 0:
+        print('Epoch [{}/{}], Loss: {:.4f}'.format(epoch + 1, 1000, loss.item()))
+
+# Save training data
+data = {
+    "model_state": model.state_dict(),
+    "input_size": inputs.shape[1],
+    "output_size": 1,
+    "inputs": inputs,
+    "targets": targets
+}
+
+FILE = "Data/data.pth"
+torch.save(data, FILE)
